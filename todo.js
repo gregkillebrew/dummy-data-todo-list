874b7ba10da3812e5d798eef30b1a289c7470588
gregkillebrew <gkillebrew91@gmail.com>
2022-06-16 10:52:34 -0500

    got it working

---
+++ todo.js
@@ -0,0 +1,42 @@
+    // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
+
+    let arrayOfTodos = [
+        {
+        "userId": 14,
+        "id": 1,
+        "title": "delectus aut autem",
+        "completed": false
+    },
+    {
+        "userId": 20,
+        "id": 2,
+        "title": "delectus aut autem",
+        "completed": false
+    }]
+    
+    const fetchTodos = () => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+        .then( (response) => response.json())
+        .then( (json) => arrayOfTodos = json)
+    }
+    
+    const logTodos = () => {
+        console.log(arrayOfTodos)
+    }
+    
+    const populateTodos = () => {
+    
+    for (i = 0; i < arrayOfTodos.length; i++) {
+
+    let toDos = document.getElementById("todo-list")
+
+    let toDoList = document.createElement("LI")
+
+    let toDoTextNode = document.createTextNode(arrayOfTodos[0].title)
+
+    toDoList.appendChild(toDoTextNode)
+
+    toDos.appendChild(toDoList)
+        }
+
+    }
